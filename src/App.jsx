689237c4bcd14a06101255ca6b443b628e90a862
Luis Hong <luishong50@gmail.com>
2023-11-02 14:51:14 -0500

    created main container and starting grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,18 @@
 import { useState } from "react";
-import UserTable from "./tables/UserTables";
-import AddUserForm from "./forms/AddUserForm";
-import EditUserForm from "./forms/EditUserForm";
 
 import "./App.css";
 
 function App() {
-    const usersData = [
-        { id: 1, name: "Tania", username: "floppydiskette" },
-        { id: 2, name: "Craig", username: "siliconeidolon" },
-        { id: 3, name: "Ben", username: "benisphere" },
-    ];
-    const [users, setUsers] = useState(usersData);
-    const [editing, setEditing] = useState(false);
-    const initialFormState = { id: null, name: "", username: "" };
-    const [currentUser, setCurrentUser] = useState(initialFormState);
-
-    const editRow = (user) => {
-        setEditing(true);
-
-        setCurrentUser({
-            id: user.id,
-            name: user.name,
-            username: user.username,
-        });
-    };
-
-    const addUser = (user) => {
-        user.id = users.length + 1;
-        setUsers([...users, user]);
-    };
-    const deleteUser = (id) => {
-        setUsers(users.filter((user) => user.id !== id));
-    };
-    const updateUser = (id, updatedUser) => {
-        setEditing(false);
-
-        setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
-    };
-
     return (
         <div className="container">
-            <h1>CRUD App with Hooks</h1>
-            <div className="flex-row">
-                <div className="flex-large">
-                    {editing ? (
-                        <div>
-                            <h2>Edit user</h2>
-                            <EditUserForm
-                                setEditing={setEditing}
-                                currentUser={currentUser}
-                                updateUser={updateUser}
-                            />
-                        </div>
-                    ) : (
-                        <div>
-                            <h2>Add user</h2>
-                            <AddUserForm addUser={addUser} />
-                        </div>
-                    )}
-                </div>
-                <div className="flex-large">
-                    <h2>View users</h2>
-                    <UserTable
-                        users={users}
-                        deleteUser={deleteUser}
-                        editRow={editRow}
-                    />
-                </div>
-            </div>
+            <div class="div1"> 1</div>
+            <div class="div2"> 2</div>
+            <div class="div3"> 3</div>
+            <div class="div4"> 4</div>
+            <div class="div5"> 5</div>
+            <div class="div6"> 6</div>
+            <div class="div7"> 7</div>
+            <div class="div8"> 8</div>
         </div>
     );
 }
