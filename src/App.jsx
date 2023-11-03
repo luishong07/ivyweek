18d3a128ac8a6a7f63ddb4461fbccb7ff4b38c2d
Luis Hong <luishong50@gmail.com>
2023-11-03 16:53:50 -0500

    adjusted spacing and started the checkboxes and inputs; redo in components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Card from "./components/Card";
 import "./App.css";
 
 function App() {
+    const date = new Date()
+    console.log(date.getDay());
 
+    //idea for background animation or progress
+    //snake-like game that grows as tasks are completed; constellation-like drawing with p5
     const weekAndDays = [
         "week",
         "mon",
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
+import "../index.css";
 
 export default function Card(props) {
-  return (
-    <div className='card'>{props.day}</div>
-  )
+    return (
+        <div className="card">
+            <div className="day-header">{props.day}</div>
+            <div className="content">
+                <div>
+                    <input type="checkbox" id="scales" name="scales"  />
+					<input type="text"/>
+                </div>
+                <div>
+                    <input type="checkbox" id="horns" name="horns" />
+					<input type="text"/>
+                </div>
+				<div>
+                    <input type="checkbox" id="horns" name="horns" />
+					<input type="text"/>
+                </div>
+				<div>
+                    <input type="checkbox" id="horns" name="horns" />
+					<input type="text"/>
+                </div>
+				<div>
+                    <input type="checkbox" id="horns" name="horns" />
+					<input type="text"/>
+                </div>
+				<div>
+                    <input type="checkbox" id="horns" name="horns" />
+					<input type="text"/>
+                </div>
+            </div>
+        </div>
+    );
 }
