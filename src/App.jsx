f0ff6b4032bfe4af7eb738f2d82b21db26765d40
Luis Hong <luishong50@gmail.com>
2023-11-17 12:02:54 -0600

    managed to add a simple test animation, but the general structure might need to be revised and reworked

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "./components/Card";
 import "./App.css";
+import anime from "animejs";
 
 function App() {
-    const date = new Date()
+    const date = new Date();
 
     //idea for background animation or progress
     //snake-like game that grows as tasks are completed; constellation-like drawing with p5
@@ -15,8 +16,21 @@ function App() {
         "thur",
         "fri",
         "sat",
-        "sun"
-    ]
+        "sun",
+    ];
+    const animationRef = useRef(null);
+    useEffect(() => {
+        animationRef.current = anime({
+            targets: ".el",
+            translateX: 550,
+            delay: function (el, i) {
+                return i * 100;
+            },
+            loop: true,
+            direction: "alternate",
+            easing: "easeInOutSine",
+        });
+    }, []);
     return (
         <div className="container">
             {weekAndDays.map(day =>{
@@ -24,6 +38,7 @@ function App() {
                     <Card key={day} day={day}/>
                 )
             })}
+            <div className="el" />
         </div>
     );
 }
