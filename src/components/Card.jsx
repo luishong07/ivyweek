f90265aa9f607a8e9ac473fd467d32bf43c92076
Luis Hong <luishong50@gmail.com>
2023-11-21 15:47:05 -0600

    added funcionality to add new task to list and conditional rendering for not showing the form once there are 6 items

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,27 @@
 import React, { useState } from "react";
 import "../index.css";
 import TaskForm from "./TaskForm";
+// import { set } from "animejs";
 
 export default function Card(props) {
     const [tasks, setTasks] = useState([
-        "groceries"
-    ])
+        "groceries",
+        // "rock",
+        // "stone",
+        // "chalupas",
+        // "tea",
+        // "msg"
+    ]);
     return (
         <div className="card">
             {/* {props.day} */}
             <div className="day-header">{props.day}</div>
             <div className="content">
-                {tasks.map((t)=>{
-                    return <div>{t}</div>
+                {tasks.map((t) => {
+                    return <div key={t}>{t}</div>;
                 })}
-                {/* <div>this</div>
-                <div>this</div>
-                <div>this</div>
-                <div>this</div>
-                <div>this</div>
-                <div>this</div> */}
 
-                {/* {
-                   itemNumbers.map(number => {
-                    return <TaskForm key={number}/>
-                   })
-                } */}
+                {tasks.length < 6 ? <TaskForm tasks={tasks} setTasks={setTasks} /> : null}
             </div>
         </div>
     );
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TaskForm() {
+export default function TaskForm(props) {
+    const [newTask, setNewTask] = useState("");
+
+    const handleAddNewTask = () =>{
+        console.log('New Task');
+        // console.log(props.tasks);
+        props.setTasks([...props.tasks,newTask])
+        setNewTask("")
+    }   
     return (
         <div>
-            <input type="checkbox"  id="taskDone"/>
-            <input type="text" id="taskInput"/>
-            <button id="saveBtn">Save</button>
+            {/* <input type="checkbox"  id="taskDone"/> */}
+            <input
+                onChange={e => setNewTask(e.target.value)}
+                value={newTask}
+                type="text"
+                id="taskInput"
+            />
+            <button onClick={handleAddNewTask} id="saveBtn">Save</button>
             {/* <button id="clearBtn">Clear</button> */}
         </div>
     );
