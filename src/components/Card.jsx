4bee5a2c8d5605f0218bd83431f561a8ff4fe67a
Luis Hong <luishong50@gmail.com>
2023-11-27 19:22:17 -0600

    with overflow hidden the bullet points disappear; work on hover overlay to see longer text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../index.css";
 import TaskForm from "./TaskForm";
 import TaskItem from "./TaskItem";
@@ -6,27 +6,59 @@ import TaskItem from "./TaskItem";
 
 export default function Card(props) {
     const [tasks, setTasks] = useState([]);
-    const handleToggle = (id) => {
 
-        let mapped = tasks.map((task) => {
-            return task.id === parseInt(id) ? { ...task, done: !task.done } : { ...task };
-        });
-        setTasks(mapped)
+    const handleFetchData = async () => {
+        const res = await fetch("http://www.boredapi.com/api/activity/")
+        const data = await res.json()
+        console.log(data);
+        let newTask = {
+            taskInfo: data.activity,
+            done: false,
+            id: tasks.length +1
+        }
+        setTasks([...tasks, newTask])
     };
 
-    const deleteTask =(id)=>{
-        let newTaskList = tasks.filter((task) =>{
-            return task.id !== parseInt(id)
-        })
-        setTasks(newTaskList)
-    }
-   
+    useEffect(() => {
+        console.log("use effect ran");
+        handleFetchData()
+    }, []);
+
+    const handleToggle = (id) => {
+        let mapped = tasks.map((task) => {
+            return task.id === parseInt(id)
+                ? { ...task, done: !task.done }
+                : { ...task };
+        });
+        setTasks(mapped);
+    };
+
+    const deleteTask = (id) => {
+        let newTaskList = tasks.filter((task) => {
+            return task.id !== parseInt(id);
+        });
+        setTasks(newTaskList);
+    };
+
     return (
         <div className="card">
-            <div className="day-header">{props.day.day }{new Date().getDay() == props.day.id ? <div className="today"> (it's today!)</div>:""}</div>
+            <div className="day-header">
+                {props.day.day}
+                {new Date().getDay() == props.day.id ? (
+                    <div className="today"> (it's today!)</div>
+                ) : (
+                    ""
+                )}
+            </div>
             <div className="content">
                 {tasks.map((t) => {
-                    return <TaskItem task={t} handleToggle={handleToggle} deleteTask={deleteTask}/>;
+                    return (
+                        <TaskItem
+                            task={t}
+                            handleToggle={handleToggle}
+                            deleteTask={deleteTask}
+                        />
+                    );
                 })}
                 {tasks.length < 6 ? (
                     <TaskForm tasks={tasks} setTasks={setTasks} />
