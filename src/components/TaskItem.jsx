56ec7d8276690fd6344e3922982c127d4ecf6749
Luis Hong <luishong50@gmail.com>
2023-11-28 19:03:36 -0600

    added tooltip on hover for when tasks are too long for the container

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 
 export default function TaskItem(props) {
-	// const []
+    // const []
 
-	const handleClick = (e)=>{
-		// console.log(e.target);
-		props.handleToggle(e.target.id)
-	}
-	const handleDelete = (e) =>{
-		props.deleteTask(e.target.id)
-	}
+    const handleClick = (e) => {
+        // console.log(e.target);
+        props.handleToggle(e.target.id);
+    };
+    const handleDelete = (e) => {
+        props.deleteTask(e.target.id);
+    };
     return (
         <div className="task-item">
-            <div 
-				id={props.task.id}
-				className={props.task.done ? "task-done" : "task"}
-				onClick={handleClick}
-			>
+            <div
+                id={props.task.id}
+                className={props.task.done ? "task-done" : "task"}
+                onClick={handleClick}
+            >
                 {props.task.taskInfo}
+                <span className="tooltiptext">{props.task.taskInfo}</span>
             </div>
-            <div 
-				id = {props.task.id} 
-				className="delete-btn"
-				onClick={handleDelete}
-				>X</div>
+            <div
+                id={props.task.id}
+                className="delete-btn"
+                onClick={handleDelete}
+            >
+                X
+            </div>
         </div>
     );
 }
