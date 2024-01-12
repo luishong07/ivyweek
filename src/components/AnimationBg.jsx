aa73a87d78ca2a4778e4a47257e4745a641c8d05
Luis Hong <luishong50@gmail.com>
2024-01-11 18:13:27 -0600

    changed values for translation

--- src/components/AnimationBg.jsx
+++ src/components/AnimationBg.jsx
@@ -11,7 +11,7 @@ export default function AnimationBg() {
     
     useEffect(() => {
         const container = document.querySelector('.anim-container')
-        for(let i = 0; i <= 48; i++){
+        for(let i = 0; i <= 7; i++){
             const blocks = document.createElement('div')
             blocks.classList.add('block')
             container.appendChild(blocks)
@@ -21,17 +21,17 @@ export default function AnimationBg() {
             anime({
                 targets: '.block',
                 translateX: function(){
-                    return anime.random(-400,400)
+                    return anime.random(-innerWidth/2,innerWidth/2)
                 },
                 translateY: function(){
-                    return anime.random(-400,400)
+                    return anime.random(-innerHeight/2,innerHeight/2)
                 },
                 scale: function(){
                     return anime.random(1,1.5)
                 },
                 easing: 'linear',
-                duration: 3000,
-                delay: anime.stagger(10),
+                duration: 4000,
+                // delay: anime.stagger(10),
                 complete: animateBlock
             })
         }
@@ -41,7 +41,7 @@ export default function AnimationBg() {
     
     return (
         <div className="animation-bg" id="animation-bg">
-            <div className="anim-container"><h2 className="center-title">Hello there</h2></div>
+            <div className="anim-container"><h2 className="center-title"></h2></div>
         </div>
     );
 }
