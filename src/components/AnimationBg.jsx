cc9356711e4d69445ca764d6ddc31bc6ff4a5887
Luis Hong <luishong50@gmail.com>
2024-01-11 18:03:56 -0600

    basic bg animation is working but the loop is a bit off;

--- src/components/AnimationBg.jsx
+++ src/components/AnimationBg.jsx
@@ -9,17 +9,39 @@ export default function AnimationBg() {
     const animation = useRef(null);
 
     
-
     useEffect(() => {
+        const container = document.querySelector('.anim-container')
+        for(let i = 0; i <= 48; i++){
+            const blocks = document.createElement('div')
+            blocks.classList.add('block')
+            container.appendChild(blocks)
+        }
        
-
-       
+        function animateBlock(){
+            anime({
+                targets: '.block',
+                translateX: function(){
+                    return anime.random(-400,400)
+                },
+                translateY: function(){
+                    return anime.random(-400,400)
+                },
+                scale: function(){
+                    return anime.random(1,1.5)
+                },
+                easing: 'linear',
+                duration: 3000,
+                delay: anime.stagger(10),
+                complete: animateBlock
+            })
+        }
+        animateBlock()
     });
 
     
     return (
         <div className="animation-bg" id="animation-bg">
-            
+            <div className="anim-container"><h2 className="center-title">Hello there</h2></div>
         </div>
     );
 }
